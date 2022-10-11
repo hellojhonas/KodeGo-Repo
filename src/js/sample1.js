523a305473ae34d17d2d9a38e7106ee6e5a7e7d0
hellojhonas <jhonacenas0704@gmail.com>
2022-10-11 20:52:01 +0800

    JavaScript code along

--- src/js/sample1.js
+++ src/js/sample1.js
@@ -1,1 +1,1 @@
-alert("Well Hello, Hello, you are entering my JavaScript intro code along!");
+// alert("Well, Hello, Hello, you are entering my JavaScript Code Along Journey!"); commenting this to have an easier coding along.
