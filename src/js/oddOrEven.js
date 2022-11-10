bdef8c6c622fe76f4f7957f992301a2245213e39
hellojhonas <jhonacenas0704@gmail.com>
2022-11-10 13:32:37 +0800

    finished the odd or even JavaScript exercise

--- src/js/oddOrEven.js
+++ src/js/oddOrEven.js
@@ -1,1 +1,16 @@
-alert("Hi, you're at the 'Odd or Even' Exercise");
+// alert("Hi, you're at the 'Odd or Even' Exercise");
+
+function oddOrEven(){
+    let num = document.getElementById('number');
+    number = parseInt(num.value);
+    if(number % 2 == 0){
+        document.getElementById('result').innerHTML ="The value that you entered: " + num.value +" is an even number";
+    }
+    
+    else if(number % 2 >= 0.01){
+        document.getElementById('result').innerHTML = "The value that you entered: " + num.value +" is an odd number";
+    }
+    else{
+        document.getElementById('result').innerHTML = "Please enter a valid whole number, thank you!";
+    }
+}
