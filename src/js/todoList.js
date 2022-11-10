ff688641c218199900d52a1edcb243189d59b29d
hellojhonas <jhonacenas0704@gmail.com>
2022-11-10 12:10:23 +0800

    created to do JavaScript exercise

--- src/js/todoList.js
+++ src/js/todoList.js
@@ -1,1 +1,13 @@
-alert("Hi, you're at the 'To Do List' Exercise");
+// alert("Hi, you're at the 'To Do List' Exercise");
+
+//a list of 8 todos
+let todos = ['JavaScript exercises', 'Coursera UX Design weekly challenge', 'GCPinas 2 quest', 'Midterm examination', 'Reply from a feedback request', 'Read more about JavaScript', 'Do some laundry', 'Prepare for an event'];
+
+//outputs only 6 todos being 'Read more about JavaScript' as the end todo
+let i = 0;
+while (i < 6){
+    let listTodos = document.createElement('li');
+    listTodos.innerText = todos[i];
+    document.querySelector('#todoList').appendChild(listTodos);
+    i++;
+}
