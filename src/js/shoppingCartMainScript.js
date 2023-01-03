3b9f5b0da9eeb077c6a50fd414098326f9f45677
hellojhonas <jhonacenas0704@gmail.com>
2023-01-03 21:39:50 +0800

    added the shopping cart

--- src/js/shoppingCartMainScript.js
+++ src/js/shoppingCartMainScript.js
@@ -1,131 +1,93 @@
-const products = [
-    {
-        "id": "1",
-        "img": "../assets/404.jpg",
-        "name": "Desktop Power 1",
-        "price": "434.00"
-    },
-    {
-        "id": "2",
-        "img": "../assets/404.jpg",
-        "name": "Laptop X",
-        "price": "1299.34"
-    },
-    {
-        "id": "3",
-        "img": "../assets/404.jpg",
-        "name": "Desktop G",
-        "price": "499.00"
-    },
-    {
-        "id": "4",
-        "img": "../assets/404.jpg",
-        "name": "Desktop Power 1",
-        "price": "434.00"
-    },
-    {
-        "id": "5",
-        "img": "../assets/404.jpg",
-        "name": "Laptop X",
-        "price": "1299.34"
+window.onload = () => {
+    let items = []
+    let cartItems = []
+
+    const fetchProducts = async () => {
+        let res = await fetch('https://api.bluecartapi.com/request?api_key=5AE04C7EFF9E4838ACCD52AD8531FB5E&search_term=highlighter+pens&type=search')
+        let json = await res.json()
+        return await json
     }
-];
 
+    const showProducts = async () => {
+        let results = await fetchProducts();
+        getItems(results['search_results']);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.onload = function () {
-    let items = [];
-    let cartItems = [];
-    getItems();
-    init();
+    showProducts()
+    init()
 
     function init() {
-        console.log("Funtion init was called.");
-        if (sessionStorage.cart) {
-            cartItems = JSON.parse(sessionStorage.getItem('cart')).items;
-        } else {
-            sessionStorage.setItem('cart', JSON.stringify({ items: [] }));
-        }
+        console.log('function init was called')
+        if (sessionStorage.cart)
+            cartItems = JSON.parse(sessionStorage.getItem('cart')).items
+        else
+            sessionStorage.setItem('cart', JSON.stringify({ items: [] }))
         updateCartIndicator();
     }
-    function getItems() {
-        items = [...products];
+
+    function getItems(products) {
+        items = products
         createItems(products);
         bindEvents();
     }
 
-    function createItems(data) {                
-        let itemList = document.querySelector('#items-list');
-        itemList.innerHTML = '';
-        let item = '';
+    function createItems(data) {
+        document.querySelector("div.page-loading-status").style = "display: none";
+        let itemsList = document.querySelector('#items-list');
+        itemsList.innerHTML = ''
+        let item = ''
         for (let i = 0; i < data.length; i++) {
-            item += '<div class="col-sm-6 col-md-4">';
-            item += '<div class="thumbnail">';
-            item += '<img src="' + data[i].img + '" alt="' + data[i].name + '" />';
-            item += '<div class="caption">';
-            item += '<h3>' + data[i].name + '<span class="label label-default">$' + data[i].price + '</span></h3>';
-             item += '<p><button data-item-id="' + data[i].id + '" class="btn btn-primary add-to-cart" role="button">Add To Cart</button></p>';
-             item += '</div></div></div>';
-         }
-         itemList.innerHTML = item;
+            item += `<div class="col m-2">
+                        <div class="thumbnail card h-100 justify-content-between">
+                        <img src='${data[i].product.images[0]}' alt='${data[i].product.title}' class="card-img-top img"/>
+                            <div class="caption p-3 d-flex flex-column justify-content-between">
+                            <h6 class="justify-content-between line-clamp">${data[i].product.title}</h6>
+                            <p>${data[i].offers.primary.currency_symbol}${data[i].offers.primary.price.toFixed(2)}</p>
+                            <button data-item-id='${data[i].position}'class='btn btn-success add-to-cart'>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>`
+        }
+        itemsList.innerHTML = item
     }
+
     function bindEvents() {
-        document.querySelectorAll('.add-to-cart').forEach(function (el) {
-            el.addEventListener('click', function (event) {
-                const id = event.target.dataset.itemId;
-                console.log('id = ' + id);
+        document.querySelectorAll('.add-to-cart').forEach(function (e) {
+            e.addEventListener('click', function (event) {
+                const id = event.target.dataset.itemId
                 const button = this;
-                button.innerHTML = 'Adding...';
-                button.setAttribute('disabled', true);
-                    setTimeout(function () {
-                        button.innerHTML = 'Add to Cart';
-                        button.removeAttribute('disabled');
-                        addItem(id);
-                        updateCartIndicator();
-                    }, 1000);
-            });
-        });
+                button.innerHTML = 'Adding...'
+                button.setAttribute('disabled', true)
+                setTimeout(() => {
+                    button.innerHTML = 'Add to Cart'
+                    button.removeAttribute('disabled')
+                    addItem(id)
+                    updateCartIndicator()
+                }, 1000)
+            })
+        })
     }
+
     function addItem(id) {
-        let itemObj = items.filter(function (item) {
-            return item.id === id;
-        })[0];
-        let itemInCart = cartItems.filter(function (product) {
-            console.log(product);
-            return product.item.id === id;
-        })[0];
-        
-        if (itemInCart) {
-            itemInCart.amount++;
-        } else {
-            cartItems.push( { item: itemObj, amount: 1});
-        }
-        sessionStorage.setItem('cart', JSON.stringify({ items: cartItems}));
+        let itemObj = items.filter(item => {
+            return item.position === parseInt(id)
+        })[0]
+        let itemInCart = cartItems.filter(item => {
+            return item.id === id
+        })[0]
+        if (itemInCart)
+            itemInCart.amount++
+        else
+            cartItems.push({ item: itemObj, amount: 1 })
+        sessionStorage.setItem('cart', JSON.stringify({ items: cartItems }))
     }
+
     function updateCartIndicator() {
-        let itemsTotal = 0;
+        let itemsTotal = 0
+        console.log('cart', cartItems)
         for (let i = 0; i < cartItems.length; i++) {
-            itemsTotal += cartItems[i].amount;
+            itemsTotal += cartItems[i].amount
         }
-        document.getElementById('cart-items-num').innerHTML = itemsTotal;
+        document.getElementById('cart-items-num').innerHTML = itemsTotal
     }
 }
-
-
-
--- src/js/shoppingCartScriptOfCart.js
+++ src/js/shoppingCartScriptOfCart.js
@@ -1,65 +1,53 @@
-window.onload = function() {
-    let cartItems = [];
-    init();
+window.onload = () => {
+    let cartItems = []
+    init()
 
     function init() {
-        cartItems = JSON.parse(sessionStorage.getItem('cart')).items || [];
-        loadItems(cartItems);
-        bindEvents();
+        cartItems = JSON.parse(sessionStorage.getItem('cart')).items || []
+        loadItems(cartItems)
+        bindEvents()
     }
-
-
-
-
-
-
-
 
     function loadItems(items) {
-        console.log(items);
-        let pEl = document.getElementById('cart-items-list');
-        pEl.innerHTML = '';
-        var itemsTotal = 0;
-        let item = '';
-        for (var i = 0; i < items.length; i++) {
-            console.log(items[i]);
-            let itemTotal = items[i].item.price * items[i].amount;
-            item += '<tr';
-            item += '<td>' + items[i].item.id + '</td>';
-            item += '<td>' + items[i].item.name + '</td>';
-            item += '<td>$' + items[i].item.price + '</td>';
-            item += '<td>' + items[i].amount + '</td>';
-            item += '<td>$' + itemTotal + '</td>';
-            item += '<td> <button data-item-id="' + items[i].item.id + '" class="btn btn-danger remove-item-btn">Remove</button></td>';
-            item += '</tr>';
-            itemsTotal += itemTotal;
+        console.log(items)
+        let pEl = document.getElementById('cart-items-list')
+        var itemsTotal = 0
+        let item = ''
+        for (let i = 0; i < items.length; i++) {
+            console.log(items[i])
+            let itemTotal = items[i].item.offers.primary.price * items[i].amount
+            console.log('total', itemTotal)
+            item += `<tr>
+            <td>${i + 1}</td>
+            <td>${items[i].item.product.title}</td>
+            <td>${items[i].item.offers.primary.price}</td>
+            <td>${items[i].amount}</td>
+            <td>${itemTotal}</td>
+            <td><button data-item-id="${items[i].item.id}" class="btn btn-danger remove-item-btn">Remove</button></td>
+            </tr>`
+            itemsTotal += itemTotal
         }
-        pEl.innerHTML = item;
-        document.getElementById('total').innerHTML = itemsTotal.toFixed(2);
+        pEl.innerHTML = item
+        document.getElementById('total').innerHTML = itemsTotal.toFixed(2)
     }
 
-
-
     function bindEvents() {
-        document.querySelectorAll(".remove-item-btn").forEach(function (el) {
+        document.querySelectorAll('.remove-item-btn').forEach(function (el) {
             el.addEventListener('click', function (event) {
-                let id = event.target.dataset.itemId;
-                console.log(id);
-                removeItem(id);
-            });
-        });
+                let id = event.target.dataset.itemId
+                removeItem(id)
+            })
+        })
     }
 
     function removeItem(id) {
         for (var i = 0; i < cartItems.length; i++) {
-            if (cartItems[i].item.id === id) {
-                cartItems.splice(i, 1);
-                sessionStorage.setItem(
-                    'cart',
-                    JSON.stringify({ items: cartItems})
-                );
-                init();
-                break;
+            if (cartItems[i].item.id === parseInt(id)) {
+                cartItems.splice(i, 1)
+                console.log(cartItems)
+                sessionStorage.setItem('cart', JSON.stringify({ items: cartItems }))
+                init()
+                break
             }
         }
     }
